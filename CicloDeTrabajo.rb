017a1a95a9311823e598d619187359e863029648
Ana Karina <ana@Isabella>
2014-02-01 04:42:35 -0430

    Committer: Ana Karina <ana@Isabella> 	new file:   CicloDeTrabajo.rb 	modified:   Contenedor.rb 	modified:   Maquina.rb 	modified:   MaquinasCerveza.rb

---
+++ CicloDeTrabajo.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby
+# == Nombre
+#
+# CicloDeTrabajo
+#
+# == Descripción
+#
+# Clase que representa el ciclo de trabajo donde se encuentra la maquina.
+#
+# == Autores
+# Wilmer Bandres (10-10055)
+# Gustavo El Khoury (10-10226)
+# Ana Arteaga (09-11059)
+# Luis Fernandes (10-10239)
+
+class CicloDeTrabajo
+  # Cantidad de ciclos.
+  attr_reader :numeroCiclo
+
+  # Construye una instancia de un CicloDeTrabajo
+  #
+  # ==== Atributos
+  #
+  # * +numeroCiclo+ - Cantidad de ciclos
+  #
+  def initialize
+    @numeroCiclo = 0
+  end
+  
+  def AumentarDeCiclo()
+    @numeroCiclo = @numeroCiclo+1
+  end
+ 
+  
+end
